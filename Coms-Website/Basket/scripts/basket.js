3828b97d41730aca462b6167deaa9432f511a3c5
Xavier Greene <xag6073@rit.edu>
2024-12-04 13:43:00 -0500

    Modified Basket to store array of items.

--- Coms-Website/Basket/scripts/basket.js
+++ Coms-Website/Basket/scripts/basket.js
@@ -1,26 +1,39 @@
 const container = document.getElementById("container");
+let item_arr = [];
+
+function onInit() {
+  let items = localStorage.getItem("items");
+  if(items == null) {
+    localStorage.setItem("items", JSON.stringify(item_arr));
+  }
+  listItems();
+}
 
 function addToBasket(item) {
-  item_num = localStorage.getItem("num");
-  if(item_num == null) item_num = 0;
+  try {
+    item_arr = JSON.parse(localStorage.getItem("items"));
+  } catch (error) {
+    console.error(error);
+    return;
+  }
 
-  localStorage.setItem(item_num, item);
+  item_arr.push(item);
   console.log("added " + item);
 
-  item_num++;
-  localStorage.setItem("num", item_num);
+  localStorage.setItem("items", JSON.stringify(item_arr));
 }
 
 function removeFromBasket(item_num) {
-  localStorage.removeItem(item_num);
+  item_arr = JSON.parse(localStorage.getItem("items"));
+  item_arr = item_arr.slice(0,item_num).concat(item_arr.slice(item_num + 1, item_arr.length));
+  localStorage.setItem("items", JSON.stringify(item_arr));
   console.log("removed " + item_num);
   listItems();
 }
 
 function empty() {
-  localStorage.clear();
-  item_num = 1;
-  localStorage.setItem("num", 0);
+  localStorage.setItem("items", JSON.stringify([]));
+  listItems();
 }
 
 function listItems() {
@@ -28,20 +41,23 @@ function listItems() {
   container.innerHTML = ""; //empty
 
   let item_count = 1;
-  item_num = localStorage.getItem("num");
-  for(let i = 0; i < item_num; i++) {
-    let item = localStorage.getItem(i);
+  item_arr = JSON.parse(localStorage.getItem("items"));
+  //if for some reason no item_arr
+  if(item_arr == null) return;
+
+  for(let i = 0; i < item_arr.length; i++) {
+    let item = item_arr[i];
     if(item !== null) {
       container.innerHTML+=`<p>${item_count}. ${item} <button onclick='removeFromBasket(${i})'>X <\p>`;
       item_count++;
     }
   }
   
-  //reset counter if empty and disable checkout button 
+  //if empty disable checkout button 
   if(item_count == 1) {
-    empty();
     
+
   }
 }
 
-window.addEventListener("DOMContentLoaded", listItems());
+window.addEventListener("DOMContentLoaded", onInit());
